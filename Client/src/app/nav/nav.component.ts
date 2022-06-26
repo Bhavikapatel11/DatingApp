cddd540a140c0d227b667d17eb5f82efc5917c5b
Bhavikapatel11 <patelbhavika1108@gmail.com>
2022-06-26 20:13:42 +0530

    End of section 6

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +12,9 @@ export class NavComponent implements OnInit {
 
   model:any = {};
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService : AccountService,
+              private route : Router,
+              private toastr : ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -18,15 +22,23 @@ export class NavComponent implements OnInit {
   login()
   {
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
-     // this.loggedIn = true;  
-      error: (error: any) => console.log(error);    
-    })    
+      this.route.navigateByUrl('/members');
+      // this.loggedIn = true;  
+    },(error: any) => {
+      console.log(error);
+      this.toastr.error(error.error);
+    }  
+    )    
+  }
+
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
   }
 
   logout(){
     //this.loggedIn = false;
     this.accountService.logout();
+    this.route.navigateByUrl('/')
   }
 
   /*it's not good way to subscribe it, so we direct call our observale using accountService with public property 
